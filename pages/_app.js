8ec8ade857556234695491e088bfda1d65724b0b
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-06-29 19:59:31 -0300

    Add highlighting to javascript code

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-
+import Prism from 'prism-react-renderer/prism'
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-javascript")
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
